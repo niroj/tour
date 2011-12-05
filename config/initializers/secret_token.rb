927da3fdef0e8a0e2e672483db2cd874cb1c6c7b
Niroj Shrestha <niroj77@yahoo.com>
2011-12-05 13:39:10 +0545

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tour::Application.config.secret_token = 'a797d99516b5c85edc802db3952650ebb229a68a5bfc0e6a18c0f43db6e25425940c4426d82a05b6dae580e92c623efaa2beccdc7a907acb805ae141546c9b42'
